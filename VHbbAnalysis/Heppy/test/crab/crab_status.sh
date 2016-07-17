cb092543550ef093e1c24a2c7d3d78253e7c8b7e
perrozzi <perrozzi@cern.ch>
2016-07-17 10:03:19 +0200

    add crab killing option

--- VHbbAnalysis/Heppy/test/crab/crab_status.sh
+++ VHbbAnalysis/Heppy/test/crab/crab_status.sh
@@ -1,6 +1,6 @@
 # usage: crab_status.sh crab_projects_V21bis_001 2>&1 | tee crab_log.log
 cd $1
-printf "Execute crab command: type \"1\" for status or \"2\" for resubmit or \"3\" for purge and press return\n"
+printf "Execute crab command: type \"1\" for status, \"2\" for resubmit, \"3\" for purge, \"4\" to kill and press return\n"
 read crab_option
 
 if [ $crab_option -eq 1 ]; then 
@@ -12,4 +12,7 @@ elif [ $crab_option -eq 2 ]; then
 elif [ $crab_option -eq 3 ]; then
   echo "crab purge inside "$PWD
   find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && crab purge -d ./" \;
+elif [ $crab_option -eq 4 ]; then
+  echo "crab killing "$PWD
+  find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && crab kill -d ./" \;
 fi
